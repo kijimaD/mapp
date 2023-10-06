400ebb22e6f7cc3703c0ae76ba110ca89cbac599
Kijima Daigo <norimaking777@gmail.com>
2023-10-06 09:26:43 +0900

    feat: use ebiten UI debug menu

--- system/renderdebug.go
+++ system/renderdebug.go
@@ -4,58 +4,75 @@ import (
 	"fmt"
 	"image/color"
 
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/kijimaD/mapp/world"
 	"github.com/sedyh/mizu/pkg/engine"
 )
 
-// デバッグテキストは左下に出る項目のこと
+// デバッグテキストは右上に出る項目のこと
 
-type renderDebugTextSystem struct {
-	op       *ebiten.DrawImageOptions
-	debugImg *ebiten.Image
-}
+type renderDebugTextSystem struct{}
 
 func NewRenderDebugTextSystem() *renderDebugTextSystem {
-	return &renderDebugTextSystem{
-		op:       &ebiten.DrawImageOptions{},
-		debugImg: ebiten.NewImage(100, 200),
-	}
+	return &renderDebugTextSystem{}
 }
 
-func (s *renderDebugTextSystem) Draw(w engine.World, screen *ebiten.Image) {
+func (r *renderDebugTextSystem) Draw(w engine.World, screen *ebiten.Image) {
 	if world.World.IsDebug == false {
 		return
 	}
-	s.debugImg.Fill(color.RGBA{0, 0, 0, 80})
+
+	r.debugmenu(w).Draw(screen)
+}
+
+func (r *renderDebugTextSystem) debugmenu(w engine.World) *ebitenui.UI {
+	face, _ := loadFont(20)
+	rootContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Padding(widget.NewInsetsSimple(20)),
+			widget.RowLayoutOpts.Spacing(4),
+		)),
+	)
+
 	mouseX, mouseY := ebiten.CursorPosition()
 	tileX, tileY := world.ScreenToCartesian(mouseX, mouseY)
-	ebitenutil.DebugPrintAt(
-		s.debugImg,
-		fmt.Sprintf(`[DEBUG]
+	labeltext := fmt.Sprintf(`[DEBUG]%s
 ENT %d
-TPS %0.0f
-FPS %0.0f
+TPS %.0f
+FPS %.0f
 Mouse (%d,%d)
 Tile (%.0f,%.0f)
 HoverType %d
 `,
-			w.Entities(),
-			ebiten.CurrentTPS(),
-			ebiten.CurrentFPS(),
-			mouseX,
-			mouseY,
-			tileX,
-			tileY,
-			world.World.HoverStructure,
-		),
-		0,
-		0,
+		"               ",
+		w.Entities(),
+		ebiten.CurrentTPS(),
+		ebiten.CurrentFPS(),
+		mouseX,
+		mouseY,
+		tileX,
+		tileY,
+		world.World.HoverStructure,
 	)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(2, 2)
-	op.GeoM.Translate(0, 800)
-	screen.DrawImage(s.debugImg, op)
-	return
+	label1 := widget.NewText(
+		widget.TextOpts.Text(labeltext, face, color.White),
+		widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionCenter),
+		widget.TextOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionEnd,
+			}),
+		),
+	)
+	label1.GetWidget().Disabled = true
+	rootContainer.AddChild(label1)
+
+	ui := ebitenui.UI{
+		Container: rootContainer,
+	}
+
+	return &ui
 }
