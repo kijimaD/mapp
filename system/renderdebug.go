5509c29f654d782e0a317c1b729368e850fc6d83
Kijima Daigo <norimaking777@gmail.com>
2023-10-10 09:29:21 +0900

    feat: デバッグメッセージはDebugPrintを使って表示する

--- system/renderdebug.go
+++ system/renderdebug.go
@@ -2,16 +2,14 @@ package system
 
 import (
 	"fmt"
-	"image/color"
 
-	"github.com/ebitenui/ebitenui"
-	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/kijimaD/mapp/world"
 	"github.com/sedyh/mizu/pkg/engine"
 )
 
-// デバッグテキストは右上に出る項目のこと
+// デバッグテキストは左上に出る項目のこと
 
 type renderDebugTextSystem struct{}
 
@@ -23,25 +21,13 @@ func (r *renderDebugTextSystem) Draw(w engine.World, screen *ebiten.Image) {
 	if world.World.IsDebug == false {
 		return
 	}
-
-	r.debugmenu(w).Draw(screen)
+	r.debugPrint(screen)
 }
 
-func (r *renderDebugTextSystem) debugmenu(w engine.World) *ebitenui.UI {
-	face, _ := loadFont(20)
-	rootContainer := widget.NewContainer(
-		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
-		widget.ContainerOpts.Layout(widget.NewRowLayout(
-			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
-			widget.RowLayoutOpts.Padding(widget.NewInsetsSimple(20)),
-			widget.RowLayoutOpts.Spacing(4),
-		)),
-	)
-
+func (r *renderDebugTextSystem) debugPrint(screen *ebiten.Image) {
 	mouseX, mouseY := ebiten.CursorPosition()
 	tileX, tileY := world.ScreenToCartesian(mouseX, mouseY)
-	labeltext := fmt.Sprintf(`[DEBUG]%s
-ENT %d
+	msg := fmt.Sprintf(`[DEBUG] - V%s
 TPS %.0f
 FPS %.0f
 Mouse (%d,%d)
@@ -49,7 +35,6 @@ Tile (%.0f,%.0f)
 HoverType %d
 `,
 		"               ",
-		w.Entities(),
 		ebiten.CurrentTPS(),
 		ebiten.CurrentFPS(),
 		mouseX,
@@ -58,21 +43,5 @@ HoverType %d
 		tileY,
 		world.World.HoverStructure,
 	)
-	label1 := widget.NewText(
-		widget.TextOpts.Text(labeltext, face, color.White),
-		widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionCenter),
-		widget.TextOpts.WidgetOpts(
-			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
-				Position: widget.RowLayoutPositionEnd,
-			}),
-		),
-	)
-	label1.GetWidget().Disabled = true
-	rootContainer.AddChild(label1)
-
-	ui := ebitenui.UI{
-		Container: rootContainer,
-	}
-
-	return &ui
+	ebitenutil.DebugPrint(screen, msg)
 }
